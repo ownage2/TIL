c9fdde752a888a23165be3133cbd52c91da0da71
DESKTOP-8GS2VKA\ownage <kjw2878@gmail.com>
2023-06-13 15:06:24 +0900

    baseball-game version 1.1

--- js/baseball-game/baseball-game.js
+++ js/baseball-game/baseball-game.js
@@ -1,83 +1,93 @@
-// 랜덤으로 생성된 숫자의 적합성 확인
-function condition(num) {
-    num = String(num);
-    let temp = num.split("");
-    // 첫번째와 두번째, 첫번째와 세번째, 두번째와 세번째의 숫자의 중복 확인
-    if (temp[0] == temp[1] || temp[0] == temp[2] || temp[1] == temp[2]) {
-        return true; // 중복이 있을 경우
-    } else if (num < 100 || num >= 1000) {
-        return true; // 생성된 수가 세자리 수가 아닐 때
-    } else {
-        return false; // 중복이 없고 세자리 수인 경우
-    }
+// // 랜덤 세자리 수 생성 함수 1 (박경덕)
+// function generateNumber () {
+//     let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//     let answer = '';
+//     for (let i = 0; i < 3; i++) {
+//       let index = Math.floor(Math.random() * numbers.length);
+//       answer += numbers[index];
+//       numbers.splice(index, 1); // numbers에서 선택된 숫자 제거
+//     }
+//     return answer;
+// }
+
+// 랜덤 세자리 수 생성 함수 2 (김정우)
+function makeTarget() {
+    let targetSet = new Set(); // Set 객체는 유일한 값을 저장할 수 있다.
+    while(targetSet.size < 3) { // 세자리 수가 될 때까지 반복
+        targetSet.add(Math.floor(Math.random() * (10 - 0) + 0)); // 0 ~ 9 까지 랜덤한 수를 targetSet에 추가
+    } 
+    return [...targetSet].join(""); // String으로 return
 }
 
-let target;
-do {
-    target = Math.floor(Math.random() * 1000); // 1000이하의 랜덤 수 생성
-} while (condition(target)) // 랜덤으로 생성된 숫자의 적합성 확인 후 반복문 종료
-// console.log(target) // target 값 출력
+// 타겟 숫자 생성
+// let target = generateNumber();
+let target = makeTarget();
+// console.log(target);
 
 // 정답 비교 함수 (볼, 스트라이크 판단)
 function compare(target, guess) {
-    let target_temp = String(target).split("");
-    let guess_temp = String(guess).split("");
+    let target_temp = target.split(""); // target 값 배열화
+    let guess_temp = guess.split(""); // guess 값 배열화
     let strikes = 0;
     let balls = 0;
 
     for (let i = 0; i < 3; i++) {
         if (guess_temp[i] === target_temp[i]) {
-            strikes++;
+            strikes++; // 위치와 값이 같을 때, strike
         } else if (target_temp.includes(guess_temp[i])) {
-            balls++;
+            balls++; // 값을 포함하고만 있을 때, ball
         }
     }
     return { balls, strikes };
 }
 
-// 게임 시작
-console.log("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!")
-
+// readline 모듈 불러오기
 const readline = require("readline");
 
+// 인터페이스 생성하기
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-let count = 0; // 시도 횟수
-let input; // 입력값
-do {
-    rl.on("line", (line) => {
+// 게임 시작
+console.log("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!")
+let count = 1; // 시도 횟수
+
+// 입력 받기
+rl.question(`${count}번째 시도 : `, function guess(input) {
+    // 입력값이 세자리 수가 아닌 경우 재시도
+    if(input.length !== 3) {
+        console.log("세자리 숫자를 입력해주세요.");
+        rl.question(`${count}번째 시도 : `, guess);
+        return;
+    }
+    
+    // 정답 비교 함수 수행 후 balls, strikes에 값 할당
+    let { balls, strikes } = compare(target, input);
+    
+    // 볼, 스트라이크를 표현
+    if (balls == 0 && strikes == 0) {
+        console.log(balls + "B" + strikes + "S");
+    } else if (balls == 0) {
+        console.log(strikes + "S");
+    } else if (strikes == 0) {
+        console.log(balls + "B");
+    } else {
+        console.log(balls + "B" + strikes + "S");
+    }
+    
+    // 3S로 맞췄을 경우 입력 종료
+    if (strikes == 3) {
+        rl.close();
+    } else { // 아닐 경우 count 더하여 재시도
         count++;
-        input = line;
-        console.log(`${count}번째 시도 : ${input}`);
-        
-        // 입력값이 세자리 수가 아닌 경우 다시 시도
-        if(input.length !== 3) {
-            console.log("세자리 숫자를 입력해주세요.");
-            count--;
-            return;
-        }
-        
-        // 정답 비교 함수 수행 후 balls, strikes에 값 할당
-        let { balls, strikes } = compare(target, Number(input));
-        
-        // 볼, 스트라이크를 표현
-        if (balls == 0 && strikes == 0) {
-            console.log(balls + "B" + strikes + "S");
-        } else if (balls == 0) {
-            console.log(strikes + "S");
-        } else if (strikes == 0) {
-            console.log(balls + "B");
-        } else {
-            console.log(balls + "B" + strikes + "S");
-        }
-        
-        // 3S로 맞췄을 경우 메시지 출력 후 종료
-        if (strikes == 3) {
-            console.log(`${count}번만에 맞히셨습니다.\n게임을 종료합니다.`);
-            rl.close();
-        }
-    })
-} while (target === input)
+        rl.question(`${count}번째 시도 : `, guess);
+    }
+})
+
+// 입력 종료 후 결과 출력
+rl.on("close", () => {
+    console.log(`${count}번만에 맞히셨습니다.\n게임을 종료합니다.`);
+	process.exit();
+});
